d9315b16dbd3614a3508df0a0a3455ee8708b5c3
Jonathan Mwaniki <idolearndaily@gmail.com>
2024-08-07 00:56:22 +0300

    refactor

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
 import prisma from "@/prisma/client";
+import { createIssueSchema } from "./createIssueSchema";
 
-const createIssueSchema = z.object({
-    title:z.string().min(1).max(255),
-    description:z.string().min(1)
-})
+
 export async function POST (request:NextRequest){
     let body = await request.json()
     let validatedIssue = createIssueSchema.safeParse(body)
