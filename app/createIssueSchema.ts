ea8baf5eea33298bc12cf9078a578c270aaac21e
Jonathan Mwaniki <idolearndaily@gmail.com>
2024-08-19 13:02:46 +0300

    client side validation

--- app/createIssueSchema.ts
+++ app/createIssueSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 const createIssueSchema = z.object({
-    title:z.string().min(1).max(255),
+    title:z.string({required_error:"Title is required"}).min(1).max(255),
     description:z.string().min(1)
 })
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,24 @@
 "use client"
 import {SimpleMdeReact} from 'react-simplemde-editor';
 import "easymde/dist/easymde.min.css";
-import { Button, TextField, Callout } from '@radix-ui/themes';
+import { Button, TextField, Callout, Text } from '@radix-ui/themes';
 import { useForm, Controller } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/createIssueSchema';
+import {z} from 'zod'
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 
-interface IssueForm{
-  title: string;
-  description:string;
-}
+type IssueForm = z.infer< typeof createIssueSchema> 
+
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState:{errors} } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
   const [error, setError] = useState("")
   const submitData = async (data: IssueForm)=>{
     try {
@@ -46,14 +50,16 @@ const NewIssuePage = () => {
     <form className='space-y-3' onSubmit={handleSubmit((data)=>submitData(data))}>
         <TextField.Root placeholder='Title' {...register("title")}>
         </TextField.Root>
+        {errors.title && <ErrorMessage>{ errors.title.message}</ErrorMessage>}
         <Controller
           name='description'
           control={control}
           render={({field})=><SimpleMdeReact placeholder='Description' {...field}/>}
           
           />
+           {errors.description && <ErrorMessage>{ errors.description.message}</ErrorMessage>}
         
-        <Button>Submit New Issue</Button>
+        <Button >Submit New Issue</Button>
     </form>
   </div>
   )
